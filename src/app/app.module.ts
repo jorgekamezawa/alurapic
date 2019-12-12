6e448afaf7ad28d906f22be0139e14ae64098ed9
Jorge Kamezawa <jorge.kamezawa@kaspper.com>
2019-12-12 17:57:33 -0300

    Aula 4 completa, parametrizaçao de rotas

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,3 +1,5 @@
+import { ErrorsModule } from './errors/errors.module';
+import { AppRoutingModule } from './app.routing.module';
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 import { AppComponent } from './app.component';
@@ -10,7 +12,9 @@ import { PhotosModule } from './photos/photos.module';
   ],
   imports: [
     BrowserModule,
-    PhotosModule
+    PhotosModule,
+    AppRoutingModule,
+    ErrorsModule
   ],
   providers: [],
   bootstrap: [AppComponent]
